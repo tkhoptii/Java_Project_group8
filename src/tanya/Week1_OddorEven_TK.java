b6793b68dc431053bd028b36b2f45b00506a058a
Owen <Owen.constantine@cydeo.com>
2024-11-15 18:01:48 +0300

    Week 1 Task_Tetiana Sample Package

---
+++ src/tanya/Week1_OddorEven_TK.java
@@ -0,0 +1,15 @@
+package tanya;
+
+
+/*
+Numbers  ---Odd & Even
+write a method which can identifies given number is even or odd
+
+
+        EX:
+             identify(5) -> "Odd"
+             identify(6) -> "Even"
+*/
+
+public class Week1_OddorEven_TK {
+}
